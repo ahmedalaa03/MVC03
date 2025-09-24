a645db9b3537f58bd07210714b08a796b1018519
Ahmed <Ahmed@AHMED>
2025-09-24 16:50:08 +0300

    Implementation of DAL

---
+++ MVC03.DAL/Data/Configration/DepartmentConfigrations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MVC03.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC03.DAL.Data.Configration
+{
+    internal class DepartmentConfigrations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.property(Id => D.Id).UseIdentityColumn(10,10);
+        }
+    }
+}
